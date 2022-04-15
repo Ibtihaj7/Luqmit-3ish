dc90afd079d6e638a300847a1e12014b13737a49
Karam47 <77400199+william94411@users.noreply.github.com>
2022-04-15 19:03:14 +0300

    -

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const app = express();
 
+app.set('view engine','hbs');
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({extended:false }));
+
+app.listen(3001, ()=>{
+    console.log("Connected in 3001 port");
+})
