803119f6067a157c9e298c5f479b3e772aca7bbd
abrarhannoon <92648528+abrarhannoon@users.noreply.github.com>
2022-04-15 23:11:42 +0300

    create database system
    create connection with database using env

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const app = express();
-require('dotenv').config();
+const dotenv=require('dotenv');
+const myenv=dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine','hbs');
 
@@ -13,3 +15,22 @@ app.use(express.urlencoded({extended:false }));
 app.listen(3001, ()=>{
     console.log("Connected in 3001 port");
 })
+
+ let databaseConnection = mysql.createConnection({
+   host: process.env.host,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database
+   
+})
+databaseConnection.connect((err)=>{
+    if(err){
+        throw err;
+    }else{
+        console.log('connected');
+    }
+})
+if (myenv.error) {
+    throw myenv.error
+  }
+  
