d2e5a0124070ec581c73c5558f1906276c0720e7
Karam47 <77400199+william94411@users.noreply.github.com>
2022-04-17 10:09:09 +0300

    .

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ const myenv=dotenv.config();
 const PORT = process.env.PORT || 3000;
 const session = require('express-session');
 
+let databaseConnection = mysql.createConnection({
+    host: process.env.host,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database
+   
+})
+databaseConnection.connect((err)=>{
+    if(err){
+        throw err;
+    }else{
+        console.log('connected');
+    }
+})
+if (myenv.error) {
+    console.log("Error while connecting the database")
+    throw myenv.error
+}
+
 app.set('view engine','hbs');
 
 const publicDirectory = path.join(__dirname,'./public');
