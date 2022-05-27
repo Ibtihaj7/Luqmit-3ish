cfe34055843f4ba24ee6e8b34d33f6878c36d913
Ibtihaj7 <ibtihaj.adham7@gmail.com>
2022-05-27 19:45:45 +0300

    some edit

--- routes/pages.js
+++ routes/pages.js
@@ -75,30 +75,26 @@ router.get('/restaurant',(req,res)=>{
             db.query("SELECT * from menu WHERE account_id = ?",[req.session.userId], (error,result)=>{
                 if(error) throw error;
                 else{ 
-                    db.query("SELECT orders.account_id,menu.discription, orders.id, account.name, orders.quantity, menu.category,orders.date FROM account JOIN orders ON account.id = orders.account_id JOIN menu ON orders.menu_id = menu.id WHERE menu.account_id =?",req.session.userId, (error,ress)=>{
-                        return res.render('resHomePage',{
-                            res1:result[0].discription,
-                            res11:result[0].quantity,
-                            res2:result[1].discription,
-                            res22:result[1].quantity,
-                            res3:result[2].discription,      
-                            res33:result[2].quantity,
-                            res4:result[3].discription,
-                            res44:result[3].quantity,
-                            res5:result[4].discription,
-                            res55:result[4].quantity,
-                            res6:result[5].discription,
-                            res66:result[5].quantity,
-                            resdata:ress
-                        })
-                    })  
+                    return res.render('resHomePage',{
+                        res1:result[0].discription,
+                        res11:result[0].quantity,
+                        res2:result[1].discription,
+                        res22:result[1].quantity,
+                        res3:result[2].discription,      
+                        res33:result[2].quantity,
+                        res4:result[3].discription,
+                        res44:result[3].quantity,
+                        res5:result[4].discription,
+                        res55:result[4].quantity,
+                        res6:result[5].discription,
+                        res66:result[5].quantity,
+                    })
                 }
             });
         }else{
             res.redirect('/charity')
         }
     })
-
 })
 router.get('/charity',(req,res)=>{
     if(!req.session.userId) return res.redirect('/endSeccion')
